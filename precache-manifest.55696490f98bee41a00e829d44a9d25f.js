790b4fb1f3f5e5383dca916cb8d1a3b5b67210aa
timazubar <timazubar33@gmail.com>
2020-06-09 16:16:58 +0300

    Updates

---
+++ precache-manifest.55696490f98bee41a00e829d44a9d25f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37be4754a8330058ba613b3791a7d29",
+    "url": "/react-course/index.html"
+  },
+  {
+    "revision": "cb3994cde6a6ccdc8139",
+    "url": "/react-course/static/css/main.1817e8f0.chunk.css"
+  },
+  {
+    "revision": "d8cc4d76d38dc18e1d57",
+    "url": "/react-course/static/js/2.80198ac0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course/static/js/2.80198ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3994cde6a6ccdc8139",
+    "url": "/react-course/static/js/main.ecb083f0.chunk.js"
+  },
+  {
+    "revision": "35420d07593cd957f972",
+    "url": "/react-course/static/js/runtime-main.8422b5ad.js"
+  }
+]);
